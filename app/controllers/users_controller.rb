9b5a8b62b6d6342e0e98c9936204dc46b02c9b2d
samroodAli <devsamrood@gmail.com>
2021-05-07 15:24:59 +0530

    User:Add create new user action logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.valid? && @user.save
+      log_in @user
+      flash[:success] = "Welcome to the club"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,4 +25,6 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
 end
