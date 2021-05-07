9f0249046ed458582f4f8959d64534aa563b9aa7
samroodAli <devsamrood@gmail.com>
2021-05-07 15:20:27 +0530

    User:Add partial for users new and edit forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/destroy'
+  get 'users/update'
+  get 'users/show'
   devise_for :users
   root 'posts#index'
   resources :users do
