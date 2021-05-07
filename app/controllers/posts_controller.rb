d6c1edd298b210b21e0c933f9d7cd8354a382309
samroodAli <devsamrood@gmail.com>
2021-05-07 14:33:17 +0530

    Post: Create strong parameters method user params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,18 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(user_params)
   end
 
   def index
+  end
+
+  private
+  def user_params
+    params.require(:post).permit(:title,:body)
   end
 end
