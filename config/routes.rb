4a2650a1347138b5fe388013a6f002d2ea665afc
samroodAli <devsamrood@gmail.com>
2021-05-07 14:15:03 +0530

    Route:Set route resources heirarchy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
   root 'posts#index'
+  resources :users do
+    resouces :posts
+  end
 end
