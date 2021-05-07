cabcd17acff34efcbad4980c58628ecfdf1e856c
samroodAli <devsamrood@gmail.com>
2021-05-07 11:54:43 +0530

    Fix:Bootstrap file rename to custom.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "bootstrap"
-import "../stylesheets/application"
+import "../stylesheets/custom"
 
 Rails.start()
 Turbolinks.start()
